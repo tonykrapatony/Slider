717738f2816afde12bc5b0921cb84a7381d2ee2c
Ihor <tonykrapatony@gmail.com>
2020-09-23 19:20:18 +0300

    descktop done

--- script/index.js
+++ script/index.js
@@ -1,11 +1,15 @@
 $(function() {
+    let current = $('.current');
+    let slides = $('.slider-nav');
+    
+
+    
     $('.slider-for').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: false,
         fade: true,
         asNavFor: '.slider-nav',
-        dots: true,
         infinite: false,
         focusOnSelect: true,
     });
@@ -15,8 +19,17 @@ $(function() {
         asNavFor: '.slider-for',
         focusOnSelect: true,
         infinite: false,
-        prevArrow: '<div class="prewArrow"><img src="./img/left-arrow.svg" alt="Left arrow icon"></div>',
-        nextArrow: '<div class="nextArrow"><img src="./img/right-arrow.svg" alt="Right arrow icon"></div>',
+        prevArrow: '<div class="prewArrow arrows"><svg width="12" height="18" viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M10 2L3 9L10 16" stroke-width="3"/></svg></div>',
+        nextArrow: '<div class="nextArrow arrows"><svg width="12" height="18" viewBox="0 0 12 18" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M2 2L9 9L2 16"  stroke-width="3"/></svg></div>',
 
     });
+    
+    $('.arrows').on('mousemove', function(){
+        $('.arrows').css('border', '2px solid #14171A');
+        $('.arrows>svg>path').css('stroke', '#14171A');
+    })
+    $('.arrows').on('mouseleave', function(){
+        $('.arrows').css('border', '2px solid #E8E8E9');
+        $('.arrows>svg>path').css('stroke', '#E8E8E9');
+    })
 });
